d6e54629d78d654309f662e60973a1427267f9e7
Antoine <antoine.lucas.33@gmail.com>
2013-06-13 20:09:02 +0200

    Fix style.css location

--- install_win.py
+++ install_win.py
@@ -7,6 +7,7 @@ moduleName = "SmithMagic"
 moduleDmName = "ExiTeD_" + moduleName + ".dm"
 moduleSwfName = moduleName + ".swf"
 moduleXmlName = "xml"
+moduleCssName = "css"
 
 srcPath = "."
 dstPath = op.normpath(op.join(os.environ['PROGRAMFILES(X86)'], "Dofus2Beta/app/ui", moduleName))
@@ -15,3 +16,5 @@ shutil.copyfile(op.normpath(op.join(srcPath, moduleDmName)), op.normpath(op.join
 shutil.copyfile(op.normpath(op.join(srcPath, moduleSwfName)), op.normpath(op.join(dstPath, moduleSwfName)))
 shutil.rmtree(op.normpath(op.join(dstPath, moduleXmlName)), 1)
 shutil.copytree(op.normpath(op.join(srcPath, moduleXmlName)), op.normpath(op.join(dstPath, moduleXmlName)))
+shutil.rmtree(op.normpath(op.join(dstPath, moduleCssName)), 1)
+shutil.copytree(op.normpath(op.join(srcPath, moduleCssName)), op.normpath(op.join(dstPath, moduleCssName)))
